893a9db5428538cc976306ea3d3ef38fc77239d0
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2025-06-25 10:39:53 +0530

    Update purchaseController.js

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -1,3 +1,20 @@
-exports.dashboard = (req, res) => {
-    res.render('dashboardPurchaser', { user: req.session.user });
+const Invoice = require('../models/Invoice');
+
+// Existing dashboard...
+
+exports.listResaleInvoices = async (req, res) => {
+    const invoices = await Invoice.find({ status: 'resale_approved' }).populate('vendor');
+    res.render('resaleMarket', { invoices });
+};
+
+exports.buyInvoice = async (req, res) => {
+    const { invoiceId } = req.body;
+    const invoice = await Invoice.findById(invoiceId);
+    if (!invoice || invoice.status !== 'resale_approved') {
+        return res.status(400).send('Invoice not available');
+    }
+    invoice.status = 'sold';
+    invoice.purchaser = req.session.user.id;
+    await invoice.save();
+    res.redirect('/purchaser/dashboard');
 };
