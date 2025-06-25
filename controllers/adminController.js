e965191c5a193e3e1f89c92bff092ca24b55e5c6
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2025-06-25 10:39:08 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,20 @@
-exports.dashboard = (req, res) => {
-    res.render('dashboardAdmin', { user: req.session.user });
+const Invoice = require('../models/Invoice');
+
+// Existing dashboard...
+
+exports.viewResaleRequests = async (req, res) => {
+    const requests = await Invoice.find({ status: 'pending_resale' }).populate('vendor');
+    res.render('resaleRequests', { requests });
+};
+
+exports.approveResale = async (req, res) => {
+    const { invoiceId } = req.body;
+    await Invoice.findByIdAndUpdate(invoiceId, { status: 'resale_approved' });
+    res.redirect('/admin/resale-requests');
+};
+
+exports.rejectResale = async (req, res) => {
+    const { invoiceId } = req.body;
+    await Invoice.findByIdAndUpdate(invoiceId, { status: 'created', resaleAmount: null });
+    res.redirect('/admin/resale-requests');
 };
